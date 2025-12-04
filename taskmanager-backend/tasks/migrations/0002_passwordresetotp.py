60a84c2d4545ea81a8ddc4a7b1c8a14c4c19ec94
Kasun Shakthi <2001shakthikasun@gmail.com>
2025-12-04 17:26:53 +0530

    Update expo version

--- taskmanager-backend/tasks/migrations/0002_passwordresetotp.py
+++ taskmanager-backend/tasks/migrations/0002_passwordresetotp.py
@@ -16,12 +16,21 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PasswordResetOTP',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID',
+                )),
                 ('otp_code', models.CharField(max_length=6)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('expires_at', models.DateTimeField()),
                 ('is_used', models.BooleanField(default=False)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='password_reset_otps', to=settings.AUTH_USER_MODEL)),
+                ('user', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='password_reset_otps',
+                    to=settings.AUTH_USER_MODEL,
+                )),
             ],
             options={
                 'ordering': ['-created_at'],
